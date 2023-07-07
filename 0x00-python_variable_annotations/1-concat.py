8a9b82adc44c9e28d391a8f10b94dc2edbeee737
NuelUzoma <emcool17@gmail.com>
2023-07-07 10:45:44 +0100

    Task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A program that takes two strings and return a concatenated string
+of both strings"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """The function to concatenate the two strings"""
+    return (str1 + str2)

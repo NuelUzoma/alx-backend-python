075f9494afe48af90e3fa34aa7bae74932962408
NuelUzoma <emcool17@gmail.com>
2023-07-07 10:52:06 +0100

    Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""A program that takes in a float and returns the floor or round number
+of the float given"""
+
+
+import math
+
+
+def floor(n: float) -> int:
+    """The function which will return the floor of the float"""
+    return math.floor(n)

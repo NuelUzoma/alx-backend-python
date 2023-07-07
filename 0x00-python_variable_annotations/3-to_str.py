f7626693dcec85f9c2d04667e89a6dc3d4530e63
NuelUzoma <emcool17@gmail.com>
2023-07-07 11:00:33 +0100

    Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A program hat takes a float n as argument and returns the string
+representation of the float."""
+
+
+def to_str(n: float) -> str:
+    """The function of the program which takes a float and returns a string"""
+    return str(float((n)))

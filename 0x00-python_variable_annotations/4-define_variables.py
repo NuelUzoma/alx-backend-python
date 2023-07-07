ec260a907b161d8fbffaeffba061b8660b7d1383
NuelUzoma <emcool17@gmail.com>
2023-07-07 11:07:22 +0100

    Task 4

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A program that defines and annotates the following variables
+with the specified values as given in the functon below"""
+
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+string: str = "Holberton"

cc20ce61a062d6ec2d0f3587fec72e491f45fccd
NuelUzoma <emcool17@gmail.com>
2023-07-10 22:25:05 +0100

    Initial Commits

---
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Write an asynchronous coroutine that takes in an integer argument
+(max_delay, with a default value of 10) named wait_random
+that wiats for a random delay between 0 and max_delay
+(included and float value) seconds and eventually return it."""
+
+
+import random
+import asyncio
+
+
+async def wait_random(max_delay: int = 10) -> float:
+    """The asynchronuos function for the program"""
+    number = random.randint(0, max_delay)
+    return float(number)
---
+++ 0x01-python_async_function/README.md
@@ -0,0 +1,18 @@
+**0x01. Python - Async**
+
+**Python**
+
+**Back-end**
+
+
+
+
+**Learning Objectives**
+
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+* async and await syntax
+* How to execute an async program with asyncio
+* How to run concurrent coroutines
+* How to create asyncio tasks
+* How to use the random module

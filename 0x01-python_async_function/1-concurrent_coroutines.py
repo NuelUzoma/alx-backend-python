11d142cbd8a7678fa5449310eca6934e61906456
NuelUzoma <emcool17@gmail.com>
2023-07-11 08:20:01 +0100

    Code Fixes

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -6,10 +6,11 @@ n times with the specified max_delay"""
 
 
 import asyncio
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> list[float]:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """The async function of the program"""
     tasks = []
     for _ in range(n):
